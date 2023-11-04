2f0f091f8158a29f7d34c73a458f01fdada706f8
yura teslyuk <116177992+yurickv@users.noreply.github.com>
2023-11-04 19:26:56 +0200

    start rest api

---
+++ Rest_API_01_auth/server.js
@@ -0,0 +1,16 @@
+const { Client } = require("pg");
+const app = require("./app");
+
+const client = new Client({
+  ssl: { rejectUnauthorized: false },
+  user: "postgres",
+  host: "localhost",
+  database: "users",
+  password: "0286",
+  port: 5432,
+});
+client.connect(function (err) {
+  if (err) throw err;
+  app.listen(5432);
+  console.log("Connected!");
+});
